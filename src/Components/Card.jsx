cf910e24ec95ac25584d552cfee52c584274696f
Fabrizio Meloni <fabrimeloni1@gmail.com>
2023-06-28 22:49:53 -0300

    home y contact

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,7 +1,7 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-
-const Card = ({ name, username, id }) => {
+const Card = ({ dentist }) => {
 
   const addFav = ()=>{
     // Aqui iria la logica para agregar la Card en el localStorage
@@ -9,6 +9,17 @@ const Card = ({ name, username, id }) => {
 
   return (
     <div className="card">
+
+        <div>
+            <h2>{dentist.name}</h2>
+            <p>{dentist.username}</p>
+            <p>{dentist.id}</p>
+
+            <Link to={`/dentist/${dentist.id}`}>
+            ir
+            </Link>
+          </div>
+
         {/* En cada card deberan mostrar en name - username y el id */}
 
         {/* No debes olvidar que la Card a su vez servira como Link hacia la pagina de detalle */}
--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 
 const Form = () => {
-  //Aqui deberan implementar el form completo con sus validaciones
+  const [name, setName] = useState('')
+  const [email,setEmail] = useState('')
+
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  const [message, setMessage] = useState("")
+
+  const handleSubmit = (e) => {
+   e.preventDefault();
+   if(name.length <= 5 || !emailRegex.test(email)){
+    setMessage("Por favor verifique su informacion nuevamente")
+   }else
+      setMessage(`Gracias ${name},${email}`)
+
+  }
 
   return (
     <div>
-      <form>
+      <h2>Want to know more?</h2>
+      <p>Send us your questions and we will contact you</p>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={name} onChange={e => setName(e.target.value)} />
+        <input type="email" value={email} onChange={e => setEmail(e.target.value)} />
+        <button type='submit'>Enviar</button>
+        <p>{message }</p>
       </form>
     </div>
   );
